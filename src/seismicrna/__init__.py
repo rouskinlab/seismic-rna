5e843e32c1d313ec7a3568d04fb32a440f8b7d2d
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-07-15 21:23:11 -0400

    Add __version__ attribute of package

--- src/seismicrna/__init__.py
+++ src/seismicrna/__init__.py
@@ -1,6 +1,13 @@
 import warnings
+from importlib import metadata
 
 from . import demult, align, relate, cluster, table, draw
 from .main import run, main_cli
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+try:
+    __version__ = metadata.version(__package__ or __name__)
+except metadata.PackageNotFoundError:
+    __version__ = None
