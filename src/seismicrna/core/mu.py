3e9b24b84501fec368bf120ff67d12f167380a12
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-08-22 23:15:04 -0400

    Add unit tests for the remaining functions in module core.mu

--- src/seismicrna/core/mu.py
+++ src/seismicrna/core/mu.py
@@ -61,6 +61,7 @@ with SEISMIC-RNA. If not, see https://www.gnu.org/licenses/.
 """
 
 from logging import getLogger
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -460,20 +461,38 @@ def calc_mu_adj_series(mu_obs: pd.Series, section: Section, min_gap: int):
     return calc_mu_adj_df(mu_obs.to_frame(), section, min_gap).squeeze(axis=1)
 
 
-def get_mu_quantile(mus: pd.Series, quantile: float) -> float:
+def get_mu_quantile(mus: np.ndarray | pd.Series, quantile: float) -> float:
     """ Compute the mutation rate at a quantile. """
-    return np.nanquantile(mus.values, quantile)
+    if mus.size == 0:
+        # If there are no values, then return NaN instead of raising an
+        # error, as np.nanquantile would.
+        value = np.nan
+    else:
+        with warnings.catch_warnings():
+            # Temporarily ignore warnings about all-NaN arrays.
+            warnings.simplefilter("ignore")
+            # Determine the quantile or, if the array is all NaN, then
+            # set value to NaN.
+            value = np.nanquantile(mus, quantile)
+    if np.isnan(value):
+        # If a NaN value was returned for either of the above reasons,
+        # then issue a warning.
+        logger.warning(f"Got NaN quantile {quantile} of {mus}")
+    return value
 
 
-def normalize(mus: pd.Series, quantile: float):
+def normalize(mus: np.ndarray | pd.Series, quantile: float):
     """ Normalize the mutation rates to a quantile. """
-    if quantile == -1.:
-        # Do not normalize the mutation rates if quantile == -1.
+    if quantile == 0.:
+        # Do not normalize the mutation rates if quantile == 0.
         return mus.copy()
     return mus / get_mu_quantile(mus, quantile)
 
 
-def winsorize(mus: pd.Series, quantile: float):
+def winsorize(mus: np.ndarray | pd.Series, quantile: float):
     """ Normalize and winsorize the mutation rates to a quantile. """
-    return pd.Series(np.clip(normalize(mus, quantile), 0., 1.),
-                     index=mus.index)
+    winsorized = np.clip(normalize(mus, quantile), 0., 1.)
+    # Return the same data type as was given for mus.
+    if isinstance(mus, pd.Series):
+        return pd.Series(winsorized, index=mus.index)
+    return winsorized
--- src/seismicrna/core/mu_test.py
+++ src/seismicrna/core/mu_test.py
@@ -34,7 +34,8 @@ import pandas as pd
 from . import mu
 from .mu import (MAX_MU, clip, _calc_mu_obs,
                  calc_mu_adj_numpy, calc_mu_adj_df, calc_mu_adj_series,
-                 calc_f_obs_numpy, calc_f_obs_df, calc_f_obs_series)
+                 calc_f_obs_numpy, calc_f_obs_df, calc_f_obs_series,
+                 get_mu_quantile, normalize, winsorize)
 from .random import rng
 from .sect import seq_pos_to_index, Section
 from .seq import DNA
@@ -465,8 +466,8 @@ class TestCalcDataFrame(ut.TestCase):
                         mus_adj_np = calc_mu_adj_numpy(mus_obs_np, gap)
                         # Compare the results.
                         self.assertIsInstance(mus_adj_df, pd.DataFrame)
-                        self.assertTrue(np.array_equal(mus_adj_df.values,
-                                                       mus_adj_np[pos]))
+                        self.assertTrue(np.allclose(mus_adj_df.values,
+                                                    mus_adj_np[pos]))
                         self.assertTrue(index.equals(mus_adj_df.index))
                         self.assertTrue(clusters.equals(mus_adj_df.columns))
                         # Run calc_f_obs_df.
@@ -475,8 +476,8 @@ class TestCalcDataFrame(ut.TestCase):
                         f_obs_np = calc_f_obs_numpy(mus_adj_np, gap)
                         # Compare the results.
                         self.assertIsInstance(f_obs_df, pd.Series)
-                        self.assertTrue(np.array_equal(f_obs_df.values,
-                                                       f_obs_np))
+                        self.assertTrue(np.allclose(f_obs_df.values,
+                                                    f_obs_np))
                         self.assertTrue(clusters.equals(f_obs_df.index))
 
 
@@ -528,3 +529,157 @@ class TestCalcSeries(ut.TestCase):
                     self.assertIsInstance(f_obs_series, float)
                     self.assertIsInstance(f_obs_np, float)
                     self.assertEqual(f_obs_series, f_obs_np)
+
+
+class TestGetMuQuantile(ut.TestCase):
+    """ Test the function `mu.get_mu_quantile`. """
+
+    class NanFilter(Filter):
+        """ Suppress warnings about NaN quantiles. """
+
+        def filter(self, rec: LogRecord):
+            """ Suppress warnings about NaN quantiles. """
+            return not rec.msg.startswith("Got NaN quantile")
+
+    nan_filter = NanFilter()
+
+    def test_no_nan(self):
+        """ Test with no NaN values. """
+        for n in [5, 11, 19]:
+            # Create a random order so that the NaN values are mixed in
+            # with the finite values.
+            order = np.arange(n)
+            rng.shuffle(order)
+            # Define quantiles of the array to check.
+            quantiles = np.linspace(0., 1., n)
+            for mu_max in np.linspace(0., 1., n):
+                # Create an array of mutation rates from 0 to mu_max and
+                # shuffle the values.
+                mus = np.linspace(0., mu_max, n)[order]
+                # Determine the value associated with each quantile.
+                values = np.array([get_mu_quantile(mus, quantile)
+                                   for quantile in quantiles])
+                # Since the values of mus were obtained via np.linspace,
+                # the value for each quantile should be the quantile
+                # times the maximum value of mus.
+                self.assertTrue(np.allclose(values, quantiles * mu_max))
+
+    def test_some_nan(self):
+        """ Test with some (but not all) NaN values. """
+        for n in [5, 11, 19]:
+            for n_nan in [1, 3, 5]:
+                # Create a random order so that the NaN values are mixed
+                # in with the finite values.
+                order = np.arange(n + n_nan)
+                rng.shuffle(order)
+                # Define quantiles of the array to check.
+                quantiles = np.linspace(0., 1., n)
+                # Test different maximum mutation rates.
+                for mu_max in np.linspace(0., 1., n):
+                    # Create an array of mutation rates from 0 to mu_max
+                    # and shuffle the values.
+                    mus = np.concatenate([np.linspace(0., mu_max, n),
+                                          np.full(n_nan, np.nan)])[order]
+                    # Determine the value associated with each quantile.
+                    values = np.array([get_mu_quantile(mus, quantile)
+                                       for quantile in quantiles])
+                    # Because the finite values of mus were obtained via
+                    # np.linspace, the value for each quantile should be
+                    # the quantile times the maximum value of mus.
+                    self.assertTrue(np.allclose(values, quantiles * mu_max))
+
+    def test_all_nan(self):
+        """ Test that an all-NaN array returns NaN values. """
+        for n in [5, 11, 19]:
+            quantiles = np.linspace(0., 1., n)
+            # Make mus an all-NaN array.
+            mus = np.full(n, np.nan)
+            # Temporarily suppress warnings about NaN values.
+            mu_logger.addFilter(self.nan_filter)
+            try:
+                values = np.array([get_mu_quantile(mus, quantile)
+                                   for quantile in quantiles])
+            finally:
+                # Re-enable warnings about NaN values.
+                mu_logger.removeFilter(self.nan_filter)
+            # Test that all quantile values are NaN.
+            self.assertTrue(np.all(np.isnan(values)))
+
+    def test_empty(self):
+        """ Test that an empty array always returns NaN values. """
+        # Make mus an empty array.
+        mus = np.array([], dtype=float)
+        for n in [5, 11, 19]:
+            quantiles = np.linspace(0., 1., n)
+            # Temporarily suppress warnings about NaN values.
+            mu_logger.addFilter(self.nan_filter)
+            try:
+                values = np.array([get_mu_quantile(mus, quantile)
+                                   for quantile in quantiles])
+            finally:
+                # Re-enable warnings about NaN values.
+                mu_logger.removeFilter(self.nan_filter)
+            # Test that all quantile values are NaN.
+            self.assertTrue(np.all(np.isnan(values)))
+
+    def test_invalid_quantiles(self):
+        """ Test that invalid quantiles raise errors. """
+        n = 11
+        mus = rng.random(n)
+        errmsg = "Quantiles must be in the range [[]0, 1[]]"
+        # Test that negative quantiles are invalid.
+        for quantile in np.linspace(0., -1., n)[1:]:
+            self.assertRaisesRegex(ValueError, errmsg,
+                                   get_mu_quantile, mus, quantile)
+        # Test that quantiles greater than 1 are invalid.
+        for quantile in np.linspace(1., 2., n)[1:]:
+            self.assertRaisesRegex(ValueError, errmsg,
+                                   get_mu_quantile, mus, quantile)
+        # Test that NaN is an invalid quantile.
+        self.assertRaisesRegex(ValueError, errmsg,
+                               get_mu_quantile, mus, np.nan)
+
+
+class TestNormalize(ut.TestCase):
+    """ Test the function `mu.normalize`. """
+
+    def test_normalize_p0(self):
+        """ Do not normalize. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(normalize(mus, 0.0), mus))
+
+    def test_normalize_p50(self):
+        """ Normalize to the median. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(normalize(mus, 0.5), mus * 20.))
+
+    def test_normalize_p100(self):
+        """ Normalize to the maximum. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(normalize(mus, 1.0), mus * 10.))
+
+
+class TestWinsorize(ut.TestCase):
+    """ Test the function `mu.winsorize`. """
+
+    def test_winsorize_p0(self):
+        """ Do not winsorize. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(winsorize(mus, 0.0), mus))
+
+    def test_winsorize_p50(self):
+        """ Winsorize to the median. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(winsorize(mus, 0.5),
+                                        np.where(mus < 0.05, mus * 20., 1.)))
+
+    def test_winsorize_p100(self):
+        """ Winsorize to the maximum. """
+        for n in [5, 12, 19]:
+            mus = np.linspace(0.0, 0.1, n)
+            self.assertTrue(np.allclose(winsorize(mus, 1.0), mus * 10.))
