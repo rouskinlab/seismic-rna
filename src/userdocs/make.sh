9a0c855f93117ba05b84126a1e4eabf4773a4058
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-08-31 16:12:11 -0400

    Add .nojekyll file

--- src/userdocs/make.sh
+++ src/userdocs/make.sh
@@ -19,3 +19,9 @@ sphinx-apidoc -s rst --no-toc --no-headings --module-first -o $API_DIR $MOD_DIR
 
 # Build the GitHub Pages files from the source files.
 sphinx-build -b html $DOC_DIR $GHP_DIR
+
+# Make an empty file called .nojekyll to tell GitHub Pages to not use Jekyll.
+# Otherwise, some files including the style sheets are not copied.
+# See https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
+touch $GHP_DIR/.nojekyll
+
