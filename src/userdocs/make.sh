0737790293b8938b2732317b6d14b7effb4971d6
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-08-31 21:54:11 -0400

    Update make.sh

--- src/userdocs/make.sh
+++ src/userdocs/make.sh
@@ -4,34 +4,34 @@ set -eu -o pipefail
 
 
 # Directories
-TOP_DIR=$HOME/git/seismic-rna
-GHP_DIR=$TOP_DIR/docs
-IMG_DIR=$TOP_DIR/logo
-SRC_DIR=$TOP_DIR/src
-MOD_DIR=$SRC_DIR/seismicrna
-DOC_DIR=$SRC_DIR/userdocs
-API_DIR=$DOC_DIR/api
+ROOT_DIR=$HOME/git/seismic-rna
+BUILD_DIR=$ROOT_DIR/docs
+LOGO_DIR=$ROOT_DIR/logo
+SRC_DIR=$ROOT_DIR/src
+SEISMIC_DIR=$SRC_DIR/seismicrna
+DOCSRC_DIR=$SRC_DIR/userdocs
+PYAPI_DIR=$DOCSRC_DIR/api
 
 # Delete the old Python API source files, if any.
-if [ -d $API_DIR ]
+if [ -d $PYAPI_DIR ]
 then
-    rm -rv $API_DIR/*
+    rm -rv $PYAPI_DIR/*
 fi
 
 # Build the Python API source files.
-sphinx-apidoc -s rst --no-toc --no-headings --module-first -o $API_DIR $MOD_DIR
+sphinx-apidoc -s rst --no-toc --no-headings --module-first -o $PYAPI_DIR $SEISMIC_DIR
 
 # Delete the old GitHub Pages files, if any.
-if [ -d $GHP_DIR ]
+if [ -d $BUILD_DIR ]
 then
-    rm -rv $GHP_DIR/*
+    rm -rv $BUILD_DIR/*
 fi
 
 # Make an empty file called .nojekyll to tell GitHub Pages to not use Jekyll.
 # Otherwise, some files including the style sheets are not copied.
 # See https://github.blog/2009-12-29-bypassing-jekyll-on-github-pages/
-touch $GHP_DIR/.nojekyll
+touch $BUILD_DIR/.nojekyll
 
 # Build the GitHub Pages files from the source files.
-sphinx-build -b html $DOC_DIR $GHP_DIR
+sphinx-build -b html $DOCSRC_DIR $BUILD_DIR
 
