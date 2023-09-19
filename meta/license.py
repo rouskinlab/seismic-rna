cee753722116f6487c35761dbadffafe9c5c1ef0
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-09-19 18:41:56 -0400

    Update license.py

--- meta/license.py
+++ meta/license.py
@@ -6,6 +6,10 @@ Add a summary of the license to the end of each source file.
 
 
 import os
+from logging import DEBUG, basicConfig, getLogger
+
+basicConfig(level=DEBUG)
+logger = getLogger(__name__)
 
 
 LICENSE_PY = """
@@ -36,28 +40,40 @@ LICENSES = {
 }
 
 
+IGNORE_NAME = {".git", ".idea", "__pycache__", "LICENSE"}
+
+
 def ensure_license(file: str):
     """ Ensure that a source file ends with the license. """
     # Get the license text appropriate for the file type.
     file_name, file_type = os.path.splitext(os.path.basename(file))
     if (license_text := LICENSES.get(file_type)) is None:
         # There is no license text for this file type.
+        logger.warning(f"No license text for file {file}")
         return
     # Remove any whitespace surrounding the license text.
     license_text = license_text.strip()
     # Read the file, eliminating trailing whitespace.
     with open(file) as f:
         contents = f.read().rstrip()
-    if not contents.endswith(license_text):
-        # Add the license to the end of the file, after one blank line.
-        contents = f"{contents}{os.linesep * 2}{license_text}{os.linesep}"
-        with open(file, 'w') as f:
-            f.write(contents)
+    if contents.endswith(license_text):
+        # The license already exists.
+        logger.debug(f"License already exists in file {file}")
+        return
+    # Add the license to the end of the file, after one blank line.
+    contents = f"{contents}{os.linesep * 2}{license_text}{os.linesep}"
+    #with open(file, 'w') as f:
+    #    f.write(contents)
+    logger.info(f"Added license text to file {file}")
 
 
 def ensure_licenses(directory: str):
     for name in os.listdir(directory):
         item = os.path.join(directory, name)
+        if name in IGNORE_NAME:
+            # Ignore the item.
+            logger.debug(f"Ignoring {item}")
+            continue
         if os.path.isdir(item):
             ensure_licenses(item)
         else:
@@ -65,7 +81,7 @@ def ensure_licenses(directory: str):
 
 
 def main():
-    ensure_licenses(os.path.dirname(os.path.abspath(__file__)))
+    ensure_licenses(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 if __name__ == "__main__":
