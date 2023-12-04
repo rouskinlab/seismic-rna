f5576c6ecdce8f1f144ee5432963b7d6d05c2541
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2023-12-03 19:58:37 -0500

    Implement choice to exlcude masked positions in fetch_count and fetch_ratio
    Fix another rebase conflict in table/base.py

--- src/seismicrna/table/base.py
+++ src/seismicrna/table/base.py
@@ -3,7 +3,6 @@ from functools import cached_property
 from pathlib import Path
 from typing import Any, Generator, Iterable
 
-import numpy as np
 import pandas as pd
 
 from ..core import path
@@ -54,48 +53,6 @@ REL_CODES = {
 
 # Columns of each relation-based table
 TABLE_RELS = list(REL_CODES.values())
-
-
-def get_dirichlet_params(mean: np.ndarray, variance: np.ndarray):
-    """ Get the concentration parameters of a Dirichlet distribution
-    with the given mean and variance.
-
-    Parameters
-    ----------
-    mean: np.ndarray
-        Mean of each dimension of the distribution.
-    variance: np.ndarray
-        Variance of each dimension of the distribution.
-
-    Returns
-    -------
-    np.ndarray
-        Concentration parameters of the Dirichlet distribution.
-    """
-    return mean * (mean * (1. - mean) / variance - 1.)
-
-
-def get_beta_params(mean: np.ndarray, variance: np.ndarray):
-    """ Get the concentration parameters of a beta distribution with the
-    given mean and variance.
-
-    Parameters
-    ----------
-    mean: np.ndarray
-        Mean of each dimension of the distribution.
-    variance: np.ndarray
-        Variance of each dimension of the distribution.
-
-    Returns
-    -------
-    tuple[numpy.ndarray, numpy.ndarray]
-        Concentration parameters of the beta distribution.
-    """
-    mean2 = 1. - mean
-    param0 = mean * mean2 / variance - 1.
-    param1 = param0 * mean
-    param2 = param0 * mean2
-    return param1, param2
 
 
 def get_rel_name(rel_code: str):
@@ -250,54 +207,32 @@ class RelTypeTable(Table, ABC):
         return False
 
     @classmethod
-    def _format_data(cls,
-                     data: pd.DataFrame, *,
-                     precision: int | None,
-                     squeeze: bool):
-        """ Perform final formatting on fetched data. """
-        if precision is not None:
-            # Round the data to the desired number of decimal places.
-            data = data.round(precision)
-        if squeeze:
-            # If the DataFrame has exactly one column, then return that
-            # column as a Series; otherwise, raise an error.
-            if data.columns.size != 1:
-                raise ValueError("Only data with exactly 1 column can be "
-                                 f"squeezed, but got {data.columns}")
-            data = data[data.columns[0]]
-        return data
+    @abstractmethod
+    def _get_select_kwargs(cls, **kwargs) -> dict[str, Any]:
+        """ Get the keyword arguments for header.select. """
 
     @abstractmethod
-    def _fetch_data(self,
-                    columns: pd.Index,
-                    exclude_masked: bool = False) -> pd.DataFrame:
+    def _fetch_data(self, columns: pd.Index, **kwargs) -> pd.DataFrame:
         """ Fetch data from the table. """
 
-    def fetch_count(self, *,
-                    exclude_masked: bool = False,
-                    squeeze: bool = False,
-                    **kwargs) -> pd.Series | pd.DataFrame:
+    def fetch_count(self, **kwargs) -> pd.Series | pd.DataFrame:
         """ Fetch counts of one or more columns. """
-        return self._format_data(self._fetch_data(self.header.select(**kwargs),
-                                                  exclude_masked),
-                                 precision=None,
-                                 squeeze=squeeze)
+        select_kwargs = self._get_select_kwargs(**kwargs)
+        return self._fetch_data(self.header.select(**select_kwargs), **kwargs)
 
     def fetch_ratio(self, *,
-                    exclude_masked: bool = False,
-                    squeeze: bool = False,
-                    precision: int | None = None,
                     quantile: float = 0.,
+                    precision: int | None = None,
                     **kwargs) -> pd.Series | pd.DataFrame:
         """ Fetch ratios of one or more columns. """
         # Fetch the data for the numerator.
-        numer = self._fetch_data(self.header.select(**kwargs), exclude_masked)
+        numer = self.fetch_count(**kwargs)
         # Fetch the data for the denominator.
-        denom = self._fetch_data(_get_denom_cols(numer.columns), exclude_masked)
+        denom = self._fetch_data(_get_denom_cols(numer.columns), **kwargs)
         # Compute the ratio of the numerator and the denominator.
-        return self._format_data(winsorize(numer / denom.values, quantile),
-                                 precision=precision,
-                                 squeeze=squeeze)
+        ratio = winsorize(numer / denom.values, quantile)
+        # Round the ratio to the desired precision.
+        return ratio.round(precision) if precision is not None else ratio
 
 
 # Table by Source (relate/mask/cluster) ################################
@@ -340,6 +275,10 @@ class PosTable(RelTypeTable, ABC):
     @classmethod
     def index_depth(cls):
         return len(SEQ_INDEX_NAMES)
+
+    @classmethod
+    def _get_select_kwargs(cls, *, exclude_masked: bool = False, **kwargs):
+        return kwargs
 
     @cached_property
     def range(self):
@@ -390,8 +329,9 @@ class PosTable(RelTypeTable, ABC):
         return section
 
     def _fetch_data(self,
-                    columns: pd.Index,
-                    exclude_masked: bool = False):
+                    columns: pd.Index, *,
+                    exclude_masked: bool = False,
+                    **kwargs):
         return (self.data.loc[self.unmasked, columns] if exclude_masked
                 else self.data.loc[:, columns])
 
@@ -415,245 +355,6 @@ class PosTable(RelTypeTable, ABC):
                                        order,
                                        clust)
 
-    def _compute_ci(self,
-                    confidence: float,
-                    use_ratio: bool, *,
-                    exclude_masked: bool = False,
-                    **kwargs):
-        """ Calculate the confidence intervals of counts or ratios.
-
-        Parameters
-        ----------
-        confidence: float
-            Confidence level; must be in [0, 1).
-        use_ratio: bool
-            Compute confidence intervals of the ratio, not the count.
-        exclude_masked: bool = False
-            Exclude masked positions from the output.
-        **kwargs
-            Keyword arguments for fetch methods.
-
-        Returns
-        -------
-        tuple[pandas.DataFrame, pandas.DataFrame]
-            Lower and upper bounds of the confidence interval.
-        """
-        from scipy.stats import binom
-        if not 0. <= confidence < 1.:
-            raise ValueError(
-                f"confidence level must be in [0, 1), but got {confidence}")
-        # Fetch the probability of each relationship.
-        p = self.fetch_ratio(exclude_masked=True, **kwargs)
-        # Fetch the number of reads for each relationship.
-        n = np.asarray(np.round(self._fetch_data(_get_denom_cols(p.columns),
-                                                 exclude_masked=True).values),
-                       dtype=int)
-        # Model the counts using binomial distributions.
-        lo, up = binom.interval(confidence, n, p.values)
-        # Copy the confidence interval bounds into two DataFrames.
-        index = self.unmasked if exclude_masked else self.data.index
-        lower = pd.DataFrame(np.nan, index=index, columns=p.columns)
-        upper = pd.DataFrame(np.nan, index=index, columns=p.columns)
-        lower.loc[self.unmasked] = lo / n if use_ratio else lo
-        upper.loc[self.unmasked] = up / n if use_ratio else up
-        return lower, upper
-
-    def ci_count(self, confidence: float, **kwargs):
-        """ Confidence intervals of counts, under these simplifications:
-
-        - Counts are independent of each other.
-        - Counts follow binomial distributions.
-        - Coverage counts are constant.
-
-        Parameters
-        ----------
-        confidence: float
-            Confidence level; must be in [0, 1).
-        **kwargs
-            Keyword arguments for fetch methods.
-
-        Returns
-        -------
-        tuple[pandas.DataFrame, pandas.DataFrame]
-            Lower and upper bounds of the confidence interval.
-        """
-        return self._compute_ci(confidence, False, **kwargs)
-
-    def ci_ratio(self, confidence: float, **kwargs):
-        """ Confidence intervals of ratios, under these simplifications:
-
-        - Ratios are independent of each other.
-        - Ratios follow beta distributions.
-        - Coverage counts are constant.
-
-        Parameters
-        ----------
-        confidence: float
-            Confidence level; must be in [0, 1).
-        **kwargs
-            Keyword arguments for fetch methods.
-
-        Returns
-        -------
-        tuple[pandas.DataFrame, pandas.DataFrame]
-            Lower and upper bounds of the confidence interval.
-        """
-        return self._compute_ci(confidence, True, **kwargs)
-
-    def _resample_clust(self,
-                        order: int,
-                        clust: int,
-                        n_cov: np.ndarray, *,
-                        seed: int | None = None,
-                        tol: float = 1.e-3):
-        """ Resample mutations for one cluster.
-
-        Parameters
-        ----------
-        order: int
-            Order of the cluster.
-        clust: int
-            Number of the cluster.
-        n_cov: numpy.ndarray
-            Number of reads covering each position (1-D array).
-        seed: int | None = None
-            Seed for the random number generator.
-        tol: float = 1.e-3
-            Tolerance for floating-point rounding error.
-        """
-        fetch_kwargs = dict(order=order, clust=clust, exclude_masked=True)
-        rng = np.random.default_rng(seed)
-        if n_cov.ndim != 1:
-            raise ValueError("n_cov must have exactly 1 dimension, "
-                             f"but got {n_cov.ndim}")
-        resampled = {COVER_REL: n_cov}
-        # Resample informative reads.
-        p_inf = self.fetch_ratio(rel=INFOR_REL,
-                                 squeeze=True,
-                                 **fetch_kwargs).values
-        n_inf = rng.binomial(n_cov, p_inf)
-        resampled[INFOR_REL] = n_inf
-        # Resample mutations and matches.
-        p_mut = self.fetch_ratio(rel=MUTAT_REL,
-                                 squeeze=True,
-                                 **fetch_kwargs).values
-        n_mut = rng.binomial(n_inf, p_mut)
-        resampled[MUTAT_REL] = n_mut
-        resampled[MATCH_REL] = n_inf - n_mut
-
-        def resample_multi(n_any_rel: np.ndarray,
-                           p_any_rel: np.ndarray,
-                           rel_kinds: list[str],
-                           tolerance: float):
-            """ Resample multiple kinds of relationships at once using a
-            multinomial distribution.
-
-            Parameters
-            ----------
-            n_any_rel: numpy.ndarray
-                Number of mutations of any kind at each position.
-            p_any_rel: numpy.ndarray
-                Probability that each position has any kind of mutation.
-            rel_kinds: list[str]
-                Kinds of relationships to resample.
-            tolerance: float
-                Tolerance for floating-point rounding error.
-            """
-            if n_any_rel.ndim != 1:
-                raise ValueError("n_any_rel must have exactly 1 dimension, "
-                                 f"but got {n_any_rel.ndim}")
-            if p_any_rel.ndim != 1:
-                raise ValueError("p_any_rel must have exactly 1 dimension, "
-                                 f"but got {p_any_rel.ndim}")
-            if tolerance < 0.:
-                raise ValueError(f"tolerance must be ≥ 0, but got {tolerance}")
-            # Fetch the probability of each given kind of relationship.
-            p_given_rel = self.fetch_ratio(rel=rel_kinds, **fetch_kwargs).values
-            # Make each probability conditional on a relationship of any
-            # kind by dividing by the probability of any relationship.
-            with np.errstate(divide="ignore"):
-                p_given_rel = np.nan_to_num(p_given_rel
-                                            / p_any_rel.reshape(-1, 1))
-            # Compute the probability of another type of relationship.
-            p_other_rel = 1. - p_given_rel.sum(axis=1)
-            # Due to rounding errors in floating-point arithmetic, these
-            # probabilities may have small negative values: zero them.
-            p_other_rel[np.logical_and(p_other_rel < 0.,
-                                       p_other_rel >= -tolerance)] = 0.
-            # Join all probabilities into one array.
-            p_every_rel = np.hstack([p_given_rel, p_other_rel.reshape(-1, 1)])
-            # To compensate, re-normalize the sum of each row to 1.
-            p_every_rel /= p_every_rel.sum(axis=1).reshape(-1, 1)
-            # Resample each kind of relationship.
-            n_every_rel = rng.multinomial(n_any_rel, p_every_rel)
-            # Extract the number of each kind of relationship.
-            n_each_rel = pd.DataFrame(n_every_rel[:, :len(rel_kinds)],
-                                      index=self.unmasked,
-                                      columns=rel_kinds)
-            return n_each_rel
-
-        # Resample each kind of mutation.
-        mut_kinds = [SUBST_REL, DELET_REL, INSRT_REL]
-        n_mut_kinds = resample_multi(n_mut, p_mut, mut_kinds, tol)
-        for kind, n_kind in n_mut_kinds.items():
-            resampled[kind] = n_kind.values
-        # Resample each kind of substitution.
-        sub_kinds = [SUB_A_REL, SUB_C_REL, SUB_G_REL, SUB_T_REL]
-        n_sub = n_mut_kinds[SUBST_REL].values
-        p_sub = self.fetch_ratio(rel=SUBST_REL,
-                                 squeeze=True,
-                                 **fetch_kwargs).values
-        n_sub_kinds = resample_multi(n_sub, p_sub, sub_kinds, tol)
-        for kind, n_kind in n_sub_kinds.items():
-            resampled[kind] = n_kind.values
-        # Return the resampled counts.
-        return resampled
-
-    def resample(self,
-                 fraction: float = 1., *,
-                 exclude_masked: bool = False,
-                 seed: int | None = None,
-                 max_seed: int = 4294967296):
-        """ Resample the reads and return a new DataFrame.
-
-        Parameters
-        ----------
-        fraction: float = 1.
-            Number of reads to resample, expressed as a fraction of the
-            original number of reads. Must be ≥ 0; may be > 1.
-        exclude_masked: bool = False
-            Exclude positions that have been masked.
-        seed: int | None = None
-            Seed for the random number generator.
-        max_seed: int = 4294967296
-            Maximum seed to pass to the next random number generator.
-        """
-        rng = np.random.default_rng(seed)
-        # Initialize the resampled data.
-        resampled = pd.DataFrame(np.nan,
-                                 index=(self.unmasked if exclude_masked
-                                        else self.data.index),
-                                 columns=self.data.columns)
-        for order, clust in self.header.clusts:
-            # Get the read coverage for the cluster.
-            n_cov = np.asarray(np.round(
-                fraction * self.fetch_count(rel=COVER_REL,
-                                            order=order,
-                                            clust=clust,
-                                            squeeze=True,
-                                            exclude_masked=True).values
-            ), dtype=int)
-            # Resample the other relationships for the cluster.
-            resampled_clust = self._resample_clust(order,
-                                                   clust,
-                                                   n_cov,
-                                                   seed=rng.integers(max_seed))
-            # Record the resampled data for the cluster.
-            for rel, n_rel in resampled_clust.items():
-                key = (rel, order, clust) if order > 0 else rel
-                resampled.loc[self.unmasked, key] = n_rel
-        return resampled
-
 
 class ReadTable(RelTypeTable, ABC):
     """ Table indexed by read. """
@@ -666,13 +367,15 @@ class ReadTable(RelTypeTable, ABC):
     def index_depth(cls):
         return len(RB_INDEX_NAMES)
 
+    @classmethod
+    def _get_select_kwargs(cls, **kwargs):
+        return kwargs
+
     @property
     def reads(self):
         return self.data.index.values
 
-    def _fetch_data(self,
-                    columns: pd.Index,
-                    exclude_masked: bool = False):
+    def _fetch_data(self, columns: pd.Index, **kwargs):
         return self.data.loc[:, columns]
 
 
@@ -704,8 +407,7 @@ class ProfilePosTable(PosTable, ABC):
         """ Yield RNA mutational profiles from a table. """
         for section in sections if sections is not None else [self.section]:
             for ho, hc in self.header.clusts:
-                if ((order is None or order == ho)
-                        and (clust is None or clust == hc)):
+                if (not order or order == ho) and (not clust or clust == hc):
                     name = format_clust_name(ho, hc, allow_zero=True)
                     yield RnaProfile(title=path.fill_whitespace(name),
                                      section=section,
@@ -714,8 +416,7 @@ class ProfilePosTable(PosTable, ABC):
                                      data=self.fetch_ratio(quantile=quantile,
                                                            rel=MUTAT_REL,
                                                            order=ho,
-                                                           clust=hc,
-                                                           squeeze=True))
+                                                           clust=hc))
 
 
 class MaskPosTable(MaskTable, ProfilePosTable, ABC):
