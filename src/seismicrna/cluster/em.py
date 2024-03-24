Correct the formula in the expectation step for computing joint probabilities; add unit tests

src/seismicrna/cluster/em.py
@@ -9,12 +9,19 @@ from .uniq import UniqReads
 from ..core.batch import get_length
 from ..core.dims import find_dims
 from ..core.header import index_order_clusts
-from ..core.mu import (calc_p_noclose_given_ends_numpy,
-                       calc_params_noclose,
+from ..core.mu import (calc_p_noclose_given_ends,
                        calc_params,
+                       calc_params_observed,
+                       calc_p_ends_given_noclose,
+                       calc_p_clust_given_noclose,
                        triu_log)
 
 logger = getLogger(__name__)
+
+# Define dimensions
+READS = "reads"
+POSITIONS = "positions"
+CLUSTERS = "clusters"
 
 LOG_LIKE_PRECISION = 3  # number of digits to round the log likelihood
 FIRST_ITER = 1  # number of the first iteration
@@ -83,17 +90,26 @@ def _expectation(p_mut: np.ndarray,
               ["p_mut", "p_ends", "p_clust", "end5s", "end3s"])
     # Ensure the mutation rates of masked positions are 0.
     p_mut = _zero_masked(p_mut, unmasked)
+    # Calculate the end probabilities.
+    p_noclose_given_ends = calc_p_noclose_given_ends(p_mut, min_mut_gap)
+    p_ends_given_noclose = calc_p_ends_given_noclose(p_ends,
+                                                     p_noclose_given_ends)
+    # Calculate the cluster probabilities.
+    # FIXME: check if this repeats the calculation for p_ends_given_noclose
+    p_clust_given_noclose = calc_p_clust_given_noclose(p_clust,
+                                                       p_ends,
+                                                       p_noclose_given_ends)
     # Compute the probability that a read would have no two mutations
     # too close given its end coordinates.
-    logp_noclose = triu_log(calc_p_noclose_given_ends_numpy(p_mut, min_mut_gap))
+    logp_noclose = triu_log(calc_p_noclose_given_ends(p_mut, min_mut_gap))
     # Compute the logs of the parameters.
     with np.errstate(divide="ignore"):
         # Suppress warnings about taking the log of zero, which is a
         # valid mutation rate.
         logp_mut = np.log(p_mut)
     logp_not = np.log(1. - p_mut)
-    logp_ends = triu_log(p_ends)
-    logp_clust = np.log(p_clust)
+    logp_ends_given_noclose = triu_log(p_ends_given_noclose)
+    logp_clust_given_noclose = np.log(p_clust_given_noclose)
     # For each cluster, calculate the probability that a read up to and
     # including each position would have no mutations.
     logp_nomut_incl = np.cumsum(logp_not, axis=0)
@@ -108,9 +124,9 @@ def _expectation(p_mut: np.ndarray,
     # and have no mutations; normalize by the fraction of all reads that
     # have no two mutations too close, i.e. logp_noclose[end5s, end3s].
     # 2D (unique reads x clusters)
-    logp_joint = (logp_clust[np.newaxis, :]
+    logp_joint = (logp_clust_given_noclose[np.newaxis, :]
                   + logp_nomut
-                  + logp_ends[end5s, end3s, np.newaxis]
+                  + logp_ends_given_noclose[end5s, end3s]
                   - logp_noclose[end5s, end3s])
     # For each unique read, compute the likelihood of observing it
     # (including its mutations) by adjusting the above likelihood
@@ -316,9 +332,9 @@ class EmClustering(object):
     def _max_step(self):
         """ Run the Maximization step of the EM algorithm. """
         # Estimate the parameters based on observed data.
-        (p_mut_given_noclose,
-         p_ends_given_noclose,
-         p_clust_given_noclose) = calc_params_noclose(
+        (p_mut_observed,
+         p_ends_observed,
+         p_clust_observed) = calc_params_observed(
             self.n_pos_total,
             self.order,
             self.unmasked,
@@ -328,7 +344,7 @@ class EmClustering(object):
             self.uniq_reads.counts_per_uniq,
             self.membership
         )
-        if self.iter > FIRST_ITER:
+        if self.iter > 1:
             # If this iteration is not the first, then use the previous
             # values of the parameters as the initial guesses.
             guess_p_mut = self.p_mut
@@ -341,9 +357,9 @@ class EmClustering(object):
             guess_p_clust = None
         # Update the parameters.
         self.p_mut, self.p_ends, self.p_clust = calc_params(
-            p_mut_given_noclose,
-            p_ends_given_noclose,
-            p_clust_given_noclose,
+            p_mut_observed,
+            p_ends_observed,
+            p_clust_observed,
             self.uniq_reads.min_mut_gap,
             guess_p_mut,
             guess_p_ends,

src/seismicrna/cluster/tests/em_test.py
@@ -26,10 +26,10 @@ class TestExpectation(ut.TestCase):
                                                                 unmasked,
                                                                 muts_per_pos,
                                                                 min_mut_gap)
-        # print("EXPECT MARGINALS")
-        # print(np.exp(expect_log_marginals))
-        # print("RESULT MARGINALS")
-        # print(np.exp(result_log_marginals))
+        print("EXPECT MARGINALS")
+        print(np.exp(expect_log_marginals))
+        print("RESULT MARGINALS")
+        print(np.exp(result_log_marginals))
         self.assertEqual(result_log_marginals.shape, expect_log_marginals.shape)
         self.assertTrue(np.allclose(result_log_marginals, expect_log_marginals))
         self.assertEqual(expect_memberships.shape, result_memberships.shape)
@@ -57,7 +57,7 @@ class TestExpectation(ut.TestCase):
                      expect_log_marginals,
                      expect_memberships)
 
-    def test_2pos(self):
+    def test_2pos_gap0(self):
         p_mut = np.array([[0.1],
                           [0.2]])
         p_ends = np.array([[0.2, 0.5],
@@ -67,11 +67,41 @@ class TestExpectation(ut.TestCase):
         end3s = np.array([0, 0, 1, 1, 1, 1, 1, 1])
         unmasked = np.array([0, 1])
         muts_per_pos = [np.array([1, 3, 5]), np.array([4, 5, 7])]
-        min_mut_gap = 0
         expect_log_marginals = np.log(
             [0.18, 0.02, 0.36, 0.04, 0.09, 0.01, 0.24, 0.06]
         )
         expect_memberships = np.ones((8, 1))
+        min_mut_gap = 0
+        self.compare(p_mut,
+                     p_ends,
+                     p_clust,
+                     end5s,
+                     end3s,
+                     unmasked,
+                     muts_per_pos,
+                     min_mut_gap,
+                     expect_log_marginals,
+                     expect_memberships)
+
+    def test_2pos_gap1(self):
+        p_mut = np.array([[0.1],
+                          [0.2]])
+        p_ends = np.array([[0.2, 0.5],
+                           [0.0, 0.3]])
+        p_clust = np.array([1.])
+        end5s = np.array([0, 0, 0, 0, 0, 1, 1])
+        end3s = np.array([0, 0, 1, 1, 1, 1, 1])
+        unmasked = np.array([0, 1])
+        muts_per_pos = [np.array([1, 3]), np.array([4, 6])]
+        expect_log_marginals = np.log([0.18 / 0.99,
+                                       0.02 / 0.99,
+                                       0.36 / 0.99,
+                                       0.04 / 0.99,
+                                       0.09 / 0.99,
+                                       0.24 / 0.99,
+                                       0.06 / 0.99])
+        expect_memberships = np.ones((7, 1))
+        min_mut_gap = 1
         self.compare(p_mut,
                      p_ends,
                      p_clust,
@@ -93,21 +123,21 @@ class TestExpectation(ut.TestCase):
         end3s = np.array([0, 0, 1, 1, 1, 1, 1, 1])
         unmasked = np.array([1])
         muts_per_pos = [np.array([4, 5, 7])]
-        min_mut_gap = 0
         expect_log_marginals = np.log(
             [0.20, 0.20, 0.40, 0.40, 0.10, 0.10, 0.24, 0.06]
         )
         expect_memberships = np.ones((8, 1))
-        self.compare(p_mut,
-                     p_ends,
-                     p_clust,
-                     end5s,
-                     end3s,
-                     unmasked,
-                     muts_per_pos,
-                     min_mut_gap,
-                     expect_log_marginals,
-                     expect_memberships)
+        for min_mut_gap in [0, 1]:
+            self.compare(p_mut,
+                         p_ends,
+                         p_clust,
+                         end5s,
+                         end3s,
+                         unmasked,
+                         muts_per_pos,
+                         min_mut_gap,
+                         expect_log_marginals,
+                         expect_memberships)
 
     def test_2pos_masked1(self):
         p_mut = np.array([[0.1],
@@ -119,11 +149,67 @@ class TestExpectation(ut.TestCase):
         end3s = np.array([0, 0, 1, 1, 1, 1, 1, 1])
         unmasked = np.array([0])
         muts_per_pos = [np.array([1, 3, 5])]
-        min_mut_gap = 0
         expect_log_marginals = np.log(
             [0.18, 0.02, 0.45, 0.05, 0.45, 0.05, 0.30, 0.30]
         )
         expect_memberships = np.ones((8, 1))
+        for min_mut_gap in [0, 1]:
+            self.compare(p_mut,
+                         p_ends,
+                         p_clust,
+                         end5s,
+                         end3s,
+                         unmasked,
+                         muts_per_pos,
+                         min_mut_gap,
+                         expect_log_marginals,
+                         expect_memberships)
+
+    def test_1pos_2clusters(self):
+        p_mut = np.array([[0.1, 0.2]])
+        p_ends = np.array([[1.]])
+        p_clust = np.array([0.4, 0.6])
+        end5s = np.array([0, 0])
+        end3s = np.array([0, 0])
+        unmasked = np.array([0])
+        muts_per_pos = [np.array([1])]
+        min_mut_gap = 0
+        expect_log_marginals = np.log([0.84, 0.16])
+        expect_memberships = np.array([[3. / 7., 4. / 7.],
+                                       [1. / 4., 3. / 4.]])
+        self.compare(p_mut,
+                     p_ends,
+                     p_clust,
+                     end5s,
+                     end3s,
+                     unmasked,
+                     muts_per_pos,
+                     min_mut_gap,
+                     expect_log_marginals,
+                     expect_memberships)
+
+    def test_2pos_gap0_2clusters(self):
+        p_mut = np.array([[0.1, 0.4],
+                          [0.2, 0.3]])
+        p_ends = np.array([[0.2, 0.5],
+                           [0.0, 0.3]])
+        p_clust = np.array([0.4, 0.6])
+        end5s = np.array([0, 0, 0, 0, 0, 0, 1, 1])
+        end3s = np.array([0, 0, 1, 1, 1, 1, 1, 1])
+        unmasked = np.array([0, 1])
+        muts_per_pos = [np.array([1, 3, 5]), np.array([4, 5, 7])]
+        expect_log_marginals = np.log(
+            [0.144, 0.056, 0.270, 0.100, 0.090, 0.040, 0.222, 0.078]
+        )
+        expect_memberships = np.array([[1. / 2., 1. / 2.],
+                                       [1. / 7., 6. / 7.],
+                                       [8. / 15., 7. / 15.],
+                                       [4. / 25., 21. / 25.],
+                                       [2. / 5., 3. / 5.],
+                                       [1. / 10., 9. / 10.],
+                                       [16. / 37., 21. / 37.],
+                                       [4. / 13., 9. / 13.]])
+        min_mut_gap = 0
         self.compare(p_mut,
                      p_ends,
                      p_clust,

src/seismicrna/table/calc.py
@@ -20,9 +20,9 @@ from .base import (COVER_REL,
 from ..cluster.data import ClusterMutsDataset
 from ..core.batch import END5_COORD, END3_COORD, accum_fits
 from ..core.header import ORDER_NAME, Header, make_header
-from ..core.mu import (calc_p_ends_given_noclose,
+from ..core.mu import (calc_p_ends_observed,
                        calc_p_noclose,
-                       calc_p_noclose_given_ends_numpy,
+                       calc_p_noclose_given_ends,
                        calc_params)
 from ..core.rel import RelPattern, HalfRelPattern
 from ..core.seq import Section
@@ -148,7 +148,7 @@ class Tabulator(ABC):
             end_counts = self._end_counts.values[:, np.newaxis]
         else:
             end_counts = self._end_counts.values
-        return calc_p_ends_given_noclose(
+        return calc_p_ends_observed(
             self.section.length,
             (self._end_counts.index.get_level_values(END5_COORD).values
              - self.section.end5),
@@ -362,7 +362,7 @@ def adjust_counts(table_per_pos: pd.DataFrame,
         # too close.
         p_noclose_given_clust = calc_p_noclose(
             p_ends,
-            calc_p_noclose_given_ends_numpy(p_mut, min_mut_gap)
+            calc_p_noclose_given_ends(p_mut, min_mut_gap)
         )
         # Drop the cluster dimension from the parameters.
         if p_mut.shape != (section.length, 1):
@@ -398,7 +398,7 @@ def adjust_counts(table_per_pos: pd.DataFrame,
             # have no two mutations too close.
             p_noclose_given_clust[i: j] = calc_p_noclose(
                 p_ends,
-                calc_p_noclose_given_ends_numpy(p_mut[:, i: j], min_mut_gap)
+                calc_p_noclose_given_ends(p_mut[:, i: j], min_mut_gap)
             )
     # Remove masked positions from the mutation rates.
     p_mut = p_mut[unmask]