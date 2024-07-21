add7277b8b404b4217a4382648f9a3989be905e1
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2024-07-21 12:10:26 -0400

    Create shell script that runs all unit tests and exits 0 if all succeed, otherwise exits 1

---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Run all unit tests for SEISMIC-RNA.
+# If all tests succeed, return 0; otherwise, return 1.
+
+set -euxo pipefail
+
+
+RESULTS=test-results.txt
+
+seismic +test 2> $RESULTS
+LAST=$(tail -n 1 $RESULTS)
+if [ $LAST == "OK" ];
+then
+	EXIT=0
+else
+	EXIT=1
+fi
+
+rm -r log
+rm $RESULTS
+exit $EXIT
+
