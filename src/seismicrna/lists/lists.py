b3a705ce7f55e87baf1e6537bb9469bf7d87f9cc
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2025-01-29 19:52:32 -0500

    Fix bug with missing path field in lists.py

--- src/seismicrna/lists/lists.py
+++ src/seismicrna/lists/lists.py
@@ -4,8 +4,7 @@ from ..core.table import Table
 
 def get_list_path(table: Table):
     """ Get the path to a list file corresponding to a table. """
-    path_fields = table.path_fields
+    path_fields = table.default_path_fields() | table.path_fields
     path_fields[path.CMD] = path.LIST_STEP
-    return path.buildpar(*path.REG_DIR_SEGS,
-                         path.PositionTableSeg,
-                         **path_fields)
+    print(path_fields)
+    return path.buildpar(*table.path_segs(), **path_fields)
