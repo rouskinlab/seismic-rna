d8b5c4c55ebfed9299d5cba658651704d37e3073
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2025-02-11 01:01:44 -0500

    Add FutureWarning

--- src/seismicrna/core/seq/section.py
+++ src/seismicrna/core/seq/section.py
@@ -1,3 +1,15 @@
-# This hook is needed for backwards-compatibility with version 0.21 and
-# will be removed after version 0.22.
-from .region import Region as Section
+import warnings
+
+from .region import Region
+
+
+class Section(Region):
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "Section is deprecated and will be removed in version 0.25. "
+            "Run seismic migrate to convert output files to the new format "
+            "and suppress this warning.",
+            FutureWarning
+        )
+        super().__init__(*args, **kwargs)
