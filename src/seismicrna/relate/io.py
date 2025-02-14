69f6115c18e45cc2b737cc32a93f4f86e6bd9a47
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2025-02-14 02:58:07 -0500

    Allow reads to have 0 segments; simplify iteration over reads

--- src/seismicrna/relate/io.py
+++ src/seismicrna/relate/io.py
@@ -76,62 +76,54 @@ def from_reads(reads: Iterable[tuple[str,
                ref: str,
                refseq: DNA,
                batch: int,
-               write_read_names: bool):
+               write_read_names: bool,
+               drop_empty_reads: bool = True):
     """ Accumulate reads into relation vectors. """
-    reads = iter(reads)
+    logger.routine("Began gathering reads into batches")
+    max_segs = -1
+    diff_segs = False
+    names = list()
+    seg_end5s = list()
+    seg_end3s = list()
     muts = {pos: defaultdict(list) for pos in range(1, len(refseq) + 1)}
-    try:
-        name, ((end5s, end3s), poss) = next(reads)
-    except StopIteration:
-        # There are no reads.
-        min_segs = 0
-        max_segs = 0
-        names = list()
-        seg_end5s = list()
-        seg_end3s = list()
-        logger.detail("There are no reads")
-    else:
-        # Initialize with the first read.
-        num_segs = len(end5s)
-        if len(end3s) != num_segs:
-            raise ValueError(f"Read {repr(name)} has {num_segs} 5' ends "
-                             f"and {len(end3s)} 3' ends")
-        min_segs = num_segs
-        max_segs = num_segs
-        names = [name]
-        seg_end5s = [end5s]
-        seg_end3s = [end3s]
-        for pos, rel in poss.items():
-            muts[pos][rel].append(0)
-        logger.detail(f"Read {repr(name)} has {num_segs} segments")
+    pos_dtype = fit_uint_type(len(refseq))
     # Accumulate the data for the remaining reads.
-    for (name, ((end5s, end3s), poss)) in reads:
-        # Find the number of segments and update the min/max numbers.
+    for read, (name, ((end5s, end3s), poss)) in enumerate(reads):
+        # Find the number of segments in this read.
         num_segs = len(end5s)
         if len(end3s) != num_segs:
             raise ValueError(f"Read {repr(name)} has {num_segs} 5' ends "
                              f"and {len(end3s)} 3' ends")
-        if num_segs < min_segs:
-            logger.detail(
-                f"Read {repr(name)} has {num_segs} segments (new minimum)"
-            )
-            min_segs = num_segs
-        if num_segs > max_segs:
-            logger.detail(
-                f"Read {repr(name)} has {num_segs} segments (new maximum)"
-            )
-            max_segs = num_segs
-        read = len(names)
+        # It is unlikely that a read has a different number of segments
+        # than other reads, so for efficiency, check for equality first.
+        if num_segs != max_segs:
+            # If the number of segments is not equal, then check whether
+            # this read has more or fewer segments than other reads.
+            if num_segs > max_segs:
+                # If this read is the first read, then max_segs == -1,
+                # so consider this read to have a different number of
+                # segments from other reads only if max_segs >= 0.
+                if max_segs >= 0:
+                    logger.detail(f"Read {repr(name)} has {num_segs} segments, "
+                                  f"more than {max_segs} in previous reads")
+                    diff_segs = True
+                max_segs = num_segs
+            else:
+                logger.detail(f"Read {repr(name)} has {num_segs} segments, "
+                              f"fewer than {max_segs} in previous reads")
+                diff_segs = True
         names.append(name)
         seg_end5s.append(end5s)
         seg_end3s.append(end3s)
         for pos, rel in poss.items():
             muts[pos][rel].append(read)
+    if max_segs < 0:
+        # If there were no reads, then set max_segs to 0 by default.
+        max_segs = 0
     # Ensure all reads have the same number of segments, padding with
     # extra no-coverage segments (end5=1, end3=0) as necessary.
-    logger.detail(f"All reads have ≥ {min_segs} and ≤ {max_segs} segments")
-    assert min_segs <= max_segs
-    if min_segs < max_segs:
+    logger.detail(f"The most segments in one read is {max_segs}")
+    if diff_segs:
         for end5s, end3s in zip(seg_end5s, seg_end3s, strict=True):
             num_segs = len(end5s)
             assert len(end3s) == num_segs
@@ -139,48 +131,46 @@ def from_reads(reads: Iterable[tuple[str,
                 padding = max_segs - num_segs
                 end5s.extend([1] * padding)
                 end3s.extend([0] * padding)
-        logger.detail(f"Padded reads with < {max_segs} segments")
-    # Make sure seg_end5s and seg_end3s have two dimensions and at least
-    # 1 column each (if there are 0 reads, then seg_end5s and seg_end3s
-    # will initially be 1-dimensional when converted to arrays).
-    pos_dtype = fit_uint_type(max(muts))
-    seg_end5s = np.array(seg_end5s, dtype=pos_dtype)
-    if seg_end5s.ndim < 2:
-        seg_end5s = seg_end5s[:, np.newaxis]
-    assert seg_end5s.ndim == 2
-    seg_end3s = np.array(seg_end3s, dtype=pos_dtype)
-    if seg_end3s.ndim < 2:
-        seg_end3s = seg_end3s[:, np.newaxis]
-    assert seg_end3s.ndim == 2
-    assert seg_end5s.shape == seg_end3s.shape == (len(names), max_segs)
-    # Keep only reads that have at least 1 segment with length ≥ 1.
-    keep = np.any(seg_end3s >= seg_end5s, axis=1)
-    if not keep.all():
-        # Calculate the numbers of the reads to keep.
-        keep_read_nums = np.flatnonzero(keep)
-        # Select only the 5' and 3' ends of the reads that are kept.
-        seg_end5s = seg_end5s[keep_read_nums]
-        seg_end3s = seg_end3s[keep_read_nums]
-        # Map the read numbers in muts to the new read numbers; drop the
-        # numbers of reads that were not kept.
-        keep_read_inverse = calc_inverse(keep_read_nums, verify=False)
-        for rels in muts.values():
-            # Iterate over list(rels.items()) to avoid iterating over
-            # rels.items() directly as rels is being modified.
-            for rel, curr_read_nums in list(rels.items()):
-                # Find the new index for each current read that is kept.
-                # This algorithm works the same as locate_elements() in
-                # core.array; it is reimplemented here to avoid needing
-                # to calculate keep_read_inverse repeatedly.
-                keep_curr_read_nums = np.intersect1d(curr_read_nums,
-                                                     keep_read_nums,
-                                                     assume_unique=True)
-                rels[rel] = keep_read_inverse[keep_curr_read_nums]
-        logger.detail(f"Removed {len(names) - keep_read_nums.size} reads "
-                      f"containing 0 segments with positive lengths")
-        # Select the names last so that before this, len(names) is still
-        # the original number of reads.
-        names = [names[i] for i in keep_read_nums]
+        logger.detail(f"Padded all reads to {max_segs} segments")
+    # Convert seg_end5s and seg_end3s to NumPy arrays.
+    num_reads = len(names)
+    assert len(seg_end5s) == len(seg_end3s) == num_reads
+    if num_reads > 0:
+        seg_end5s = np.array(seg_end5s, dtype=pos_dtype)
+        seg_end3s = np.array(seg_end3s, dtype=pos_dtype)
+    else:
+        seg_end5s = np.zeros((num_reads, max_segs), dtype=pos_dtype)
+        seg_end3s = np.zeros((num_reads, max_segs), dtype=pos_dtype)
+    assert seg_end5s.ndim == seg_end3s.ndim == 2
+    assert seg_end5s.shape == seg_end3s.shape == (num_reads, max_segs)
+    if drop_empty_reads:
+        # Keep only reads that have at least 1 segment with length ≥ 1.
+        keep = np.any(seg_end3s >= seg_end5s, axis=1)
+        if not keep.all():
+            # Calculate the numbers of the reads to keep.
+            keep_read_nums = np.flatnonzero(keep)
+            # Select the names and 5'/3' ends of the reads that are kept.
+            names = [names[i] for i in keep_read_nums]
+            seg_end5s = seg_end5s[keep_read_nums]
+            seg_end3s = seg_end3s[keep_read_nums]
+            # Map the read numbers in muts to the new read numbers; drop
+            # the numbers of reads that were not kept.
+            keep_read_inverse = calc_inverse(keep_read_nums, verify=False)
+            for rels in muts.values():
+                # Iterate over list(rels.items()) to avoid iterating
+                # over rels.items() directly as rels is being modified.
+                for rel, curr_read_nums in list(rels.items()):
+                    # Find the new index for each read that is kept.
+                    # This algorithm works the same as locate_elements()
+                    # in core.array; it is reimplemented here to avoid
+                    # needing to calculate keep_read_inverse repeatedly.
+                    keep_curr_read_nums = np.intersect1d(curr_read_nums,
+                                                         keep_read_nums,
+                                                         assume_unique=True)
+                    rels[rel] = keep_read_inverse[keep_curr_read_nums]
+            logger.detail(
+                f"Dropped {num_reads - keep_read_nums.size} empty reads"
+            )
     # Assemble and return the batches.
     relate_batch = RelateBatchIO(sample=sample,
                                  branches=branches,
@@ -197,4 +187,5 @@ def from_reads(reads: Iterable[tuple[str,
                                       names=names)
     else:
         name_batch = None
+    logger.routine("Ended gathering reads into batches")
     return relate_batch, name_batch
