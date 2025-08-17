a9b12d096c503fdec3025e2fd78caa3d9eac744c
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2025-08-17 01:25:22 -0400

    Add more unit tests for confusion matrix functions

--- src/seismicrna/core/batch/tests/confusion_test.py
+++ src/seismicrna/core/batch/tests/confusion_test.py
@@ -3,10 +3,136 @@ import unittest as ut
 import numpy as np
 import pandas as pd
 
-from seismicrna.core.batch.confusion import (calc_confusion_phi,
+from seismicrna.core.batch.confusion import (init_confusion_matrix,
+                                             calc_confusion_matrix,
+                                             calc_confusion_phi,
+_count_intersection,
                                              label_significant_pvals)
+from seismicrna.core.rel.code import DELET, SUB_A, SUB_C, SUB_G, SUB_T
+from seismicrna.core.rel.pattern import RelPattern
+from seismicrna.core.seq.region import POS_NAME, BASE_NAME
 
 rng = np.random.default_rng()
+
+
+class TestCountIntersection(ut.TestCase):
+
+    def test_empty(self):
+        x = np.array([])
+        y = np.array([])
+        self.assertEqual(_count_intersection(x, y), 0)
+
+    def test_0_overlap(self):
+        x = np.array([0, 2, 4, 6])
+        y = np.array([1, 3, 5])
+        self.assertEqual(_count_intersection(x, y), 0)
+        self.assertEqual(_count_intersection(y, x), 0)
+
+    def test_first_overlap(self):
+        x = np.array([1, 2, 4, 6])
+        y = np.array([1, 3, 5])
+        self.assertEqual(_count_intersection(x, y), 1)
+        self.assertEqual(_count_intersection(y, x), 1)
+
+    def test_last_overlap(self):
+        x = np.array([0, 2, 4, 6])
+        y = np.array([1, 3, 6])
+        self.assertEqual(_count_intersection(x, y), 1)
+        self.assertEqual(_count_intersection(y, x), 1)
+
+    def test_middle_overlap(self):
+        x = np.array([0, 2, 4, 6, 7, 8, 9, 10, 12, 14])
+        y = np.array([1, 3, 6, 7, 8, 9, 11, 13])
+        self.assertEqual(_count_intersection(x, y), 4)
+        self.assertEqual(_count_intersection(y, x), 4)
+
+    def test_count_intersect(self):
+        x = np.unique(rng.integers(100, size=100))
+        y = np.unique(rng.integers(100, size=100))
+        count = np.intersect1d(x, y).size
+        self.assertEqual(_count_intersection(x, y), count)
+        self.assertEqual(_count_intersection(y, x), count)
+
+
+class TestInitConfusionMatrix(ut.TestCase):
+
+    def test_no_clusters(self):
+        pos_index = pd.MultiIndex.from_arrays([[2, 4, 7, 9],
+                                               ["C", "C", "A", "A"]],
+                                              names=[POS_NAME,
+                                                     BASE_NAME])
+        expect_index = pd.MultiIndex.from_arrays([[2, 2, 2, 4, 4, 7],
+                                                  [4, 7, 9, 7, 9, 9]],
+                                                 names=["Position A",
+                                                        "Position B"])
+        n, a, b, ab = init_confusion_matrix(pos_index)
+        for entry in [n, a, b, ab]:
+            self.assertIsInstance(entry, pd.Series)
+            self.assertTrue(entry.index.equals(expect_index))
+            self.assertTrue(entry.index.equal_levels(expect_index))
+            self.assertTrue(np.all(entry == 0))
+
+
+class TestCalcConfusionMatrix(ut.TestCase):
+
+    def test_no_clusters(self):
+        """
+            Pos.
+        Rd. 2479
+        0   ====
+        2   A=C=
+        3   =G=T
+        5   .G=.
+        8   ..==
+        9   T-=.
+        """
+        pattern = RelPattern.from_counts(count_del=False, count_ins=False)
+        read_nums = np.array([0, 2, 3, 5, 8, 9])
+        pos_index = pd.MultiIndex.from_arrays([[2, 4, 7, 9],
+                                               ["C", "C", "A", "A"]],
+                                              names=[POS_NAME, BASE_NAME])
+        mutations = {2: {SUB_A: np.array([2]),
+                         SUB_T: np.array([9])},
+                     4: {SUB_G: np.array([3, 5]),
+                         DELET: np.array([9])},
+                     7: {SUB_C: np.array([2])},
+                     9: {SUB_T: np.array([3])}}
+        seg_end5s = np.array([[2, 7],
+                              [2, 4],
+                              [2, 4],
+                              [4, 4],
+                              [7, 1],
+                              [2, 4]])
+        seg_end3s = np.array([[4, 9],
+                              [7, 9],
+                              [4, 9],
+                              [7, 7],
+                              [9, 0],
+                              [4, 7]])
+        seg_ends_mask = seg_end5s > seg_end3s
+        expect_index = pd.MultiIndex.from_arrays([[2, 2, 2, 4, 4, 7],
+                                                  [4, 7, 9, 7, 9, 9]],
+                                                 names=["Position A",
+                                                        "Position B"])
+        en = pd.Series([4, 4, 3, 5, 3, 4], expect_index)
+        ea = pd.Series([2, 2, 1, 2, 1, 1], expect_index)
+        eb = pd.Series([1, 1, 1, 1, 1, 1], expect_index)
+        eab = pd.Series([0, 1, 0, 0, 1, 0], expect_index)
+        for result, expect in zip(
+                calc_confusion_matrix(pattern,
+                                      mutations,
+                                      pos_index,
+                                      read_nums,
+                                      seg_end5s,
+                                      seg_end3s,
+                                      seg_ends_mask),
+                [en, ea, eb, eab],
+                strict=True
+        ):
+            self.assertIsInstance(result, pd.Series)
+            self.assertTrue(result.index.equals(expect.index))
+            self.assertTrue(result.index.equal_levels(expect.index))
+            self.assertTrue(result.equals(expect))
 
 
 class TestCalcConfusionPhi(ut.TestCase):
