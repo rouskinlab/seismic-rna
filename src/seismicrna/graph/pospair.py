36b02c3aa49bf6b43d9ada6085eeda349874a770
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2025-08-23 14:23:26 -0400

    Use min_gap in poscorr for mask and cluster datasets

--- src/seismicrna/graph/pospair.py
+++ src/seismicrna/graph/pospair.py
@@ -7,6 +7,7 @@ from .cgroup import make_tracks
 from .dataset import DatasetGraph, DatasetWriter, DatasetRunner
 from .trace import get_pairwise_position_trace
 from ..core.batch import POSITION_A, POSITION_B, accumulate_confusion_matrices
+from ..core.dataset import UnbiasDataset
 from ..core.header import NO_CLUST, NO_CLUSTS, NUM_CLUSTS_NAME, CLUST_NAME
 
 
@@ -46,6 +47,9 @@ class PositionPairGraph(DatasetGraph, ABC):
             self.pattern,
             self.dataset.region.unmasked,
             clusters,
+            min_gap=(self.dataset.min_mut_gap
+                     if isinstance(self.dataset, UnbiasDataset)
+                     else 0),
             num_cpus=self.num_cpus
         ))
         if clusters is None:
