10ed51e6031ef4906383b28e5a443ca2f4e6cd97
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2026-05-25 02:36:41 -0400

    Fix bug causing seismic renumct to crash

--- src/seismicrna/renumct/main.py
+++ src/seismicrna/renumct/main.py
@@ -57,7 +57,7 @@ def run(
         # Otherwise, determine the output paths using out_dir.
         out_dir = path.sanitize(out_dir)
         out_dir.mkdir(parents=True, exist_ok=True)
-        out_path = path.transpaths(out_dir, *file_start, strict=True)
+        out_path = path.transpaths(out_dir, file_start, strict=True)
     file_out = dict(zip(file_start, out_path, strict=True))
     # Generate the positional arguments for renumber_ct.
     args = [(file, file_out[file], file_start[file]) for file in file_start]
