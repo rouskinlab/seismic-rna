1188d9afd3a0993b7baa2b0b82acb968120db31c
matthewfallan <31744230+matthewfallan@users.noreply.github.com>
2026-05-31 17:33:35 -0400

    Update copyright to 2026

--- src/seismicrna/__init__.py
+++ src/seismicrna/__init__.py
@@ -4,7 +4,7 @@ __all__ = ["__version__"]
 
 ########################################################################
 #                                                                      #
-# © Copyright 2022-2025, the Rouskin Lab.                              #
+# © Copyright 2022-2026, the Rouskin Lab.                              #
 #                                                                      #
 # This file is part of SEISMIC-RNA.                                    #
 #                                                                      #
